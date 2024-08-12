a6a8b12c7ec882059980bc54c8cc25431142c609
Sahananehru <142662045+Sahananehru@users.noreply.github.com>
2024-08-12 12:19:39 +0530

    Myjavafile

---
+++ JAVA/JAVAFULLSTACK/src/JAVAFS/ASCII.java
@@ -0,0 +1,11 @@
+package JAVAFS;
+import java.util.*;
+public class ASCII
+{
+  public static void main(String[] args)
+  {
+	  Scanner sc=new Scanner(System.in);
+	  char ch=sc.next().charAt(0);
+	  System.out.println((int)ch);
+  }
+}
