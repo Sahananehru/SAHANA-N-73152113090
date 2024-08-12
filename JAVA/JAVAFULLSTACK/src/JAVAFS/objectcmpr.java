4a18f5e572003301a988c5d84f38bc90dc9ff762
Sahananehru <142662045+Sahananehru@users.noreply.github.com>
2024-08-12 12:00:07 +0530

    Mycommit

--- JAVA/JAVAFULLSTACK/src/JAVAFS/objectcmpr.java
+++ JAVA/JAVAFULLSTACK/src/JAVAFS/objectcmpr.java
@@ -5,8 +5,10 @@ public class objectcmpr
   public static void main(String[] args)
   {
 	  Scanner sc=new Scanner(System.in);
-	  Double x = new Double(123.45555);     
-	  Long y = new Long(9887544); 
+//	  Double x = new Double(123.45555);
+//	  Long y = new Long(9887544); 
+	  String x=new String("Sahana");
+	  String y=new String("Sahana");
 	  if(x.equals(y))
 	  {
 		  System.out.println("Yes");
